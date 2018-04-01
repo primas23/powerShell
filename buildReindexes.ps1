254d0b0c1e1d53a47852ebf895c5602eee8f1208
Petar Petrov <petpet@cobuilder.no>
2018-04-01 22:50:31 +0300

    Added logic

--- buildReindexes.ps1
+++ buildReindexes.ps1
@@ -10,48 +10,78 @@ $pdtReindexCon = 'CoBuilder.Search.PDTReindex.exe.config'
 $pndReindexCon = 'CoBuilder.Search.PnDReindex.exe.config'
 # $pndServiceCon = 'CoBuilder.Search.PnDService.exe.config'
 
-$indexes = '<add key="documents" value="true"/>', '<add key="links" value="true"/>', '<add key="products" value="true"/>'
+$indexes = '<add key="reindexPDTs" value="true" />',
+'<add key="reindexPTs" value="true" />',
+'<add key="reindexClassifications" value="true" />',
+'<add key="reindexAccounts" value="true" />',
+'<add key="reindexProducts" value="true" />',
+'<add key="reindexDocuments" value="true" />',
+'<add key="reindexProperties" value="true" />',
+'<add key="reindexUses" value="true" />',
+'<add key="reindexStandards" value="true" />',
+'<add key="reindexProducts" value="true" />',
+'<add key="reindexDocuments" value="true" />'
 
 $partialPathProjectPath = 'D:\development\CoBuilderSearch\CoBuilder.Search.'
 $partialBinPath = '\bin\Debug'
 $buildsDir = 'D:\Temp\Builds' 
 
 # Build
-Write-Host "Restoring NuGet packages" -foregroundcolor green
-nuget restore "$($pathSln)"
+# Write-Host "Restoring NuGet packages" -foregroundcolor green
+# nuget restore "$($pathSln)"
 
-Write-Host "Cleaning $($pathSln)" -foregroundcolor green
-& "$($msBuildExe)" "$($pathSln)" /t:Clean /m
+# Write-Host "Cleaning $($pathSln)" -foregroundcolor green
+# & "$($msBuildExe)" "$($pathSln)" /t:Clean /m
 
-Write-Host "Building $($pathSln)" -foregroundcolor green
-& "$($msBuildExe)" "$($pathSln)" /t:Build /m
+# Write-Host "Building $($pathSln)" -foregroundcolor green
+# & "$($msBuildExe)" "$($pathSln)" /t:Build /m
 
 # Copying
 $pdtReindexConfContent = Get-Content "$($partialPathProjectPath)$($pdtReindex)$($partialBinPath)\$($pdtReindexCon)"
 $pdtpndReindexConfContent = Get-Content "$($partialPathProjectPath)$($pndReindex)$($partialBinPath)\$($pndReindexCon)"
 
-# TODO: Iterate indexes and make them false in pdtReindexConfContent and pdtpndReindexConfContent
-
-for ($i = 0; $i -lt $indexes.Length; $i++) {
-    if ($pdtReindexConfContent -like $("*$indexes[$i]*")) {
-        # Pdt Reindex
-        Write-Host "Copying Pdt Reindex to $($buildsDir)" -foregroundcolor green
-        & Copy-Item -Path "$($partialPathProjectPath)$($pdtReindex)$($partialBinPath)" -Destination "$($buildsDir)" -Recurse
-        & Rename-Item -Path "$($buildsDir)\Debug" "$($buildsDir)\$pdtReindex$($i)"
-        # & Remove-Item -Path "$($buildsDir)\$pdtReindex\$pdtReindexCon"
-    }
-
-    if ($pdtpndReindexConfContent -like $("*$indexes[$i]*")) {
-        # Pnd Reindex
-        Write-Host "Copying Pnd Reindex to $($buildsDir)" -foregroundcolor green
-        & Copy-Item -Path "$($partialPathProjectPath)$($pndReindex)$($partialBinPath)" -Destination "$($buildsDir)" -Recurse
-        & Rename-Item -Path "$($buildsDir)\Debug" "$($buildsDir)\$pndReindex$($i)"
-        # & Remove-Item -Path "$($buildsDir)\$pndReindex\$pndReindexCon"
-    }
+if ($pdtReindexConfContent -like $("*$indexes[1]*")) {
+    Write-Host 'primas'
+}
+else {
+    Write-Host 'kur'
+    Write-Host $indexes[1]
+    Write-Host $pdtReindexConfContent
 }
 
+# for ($i = 0; $i -lt $indexes.Length; $i++) {
+#     if ($pdtReindexConfContent -like $("*$indexes[$i]*")) {
+
+#         # $pdtReindexConfContent.Replace("*$indexes[$i]*", "*$indexes[$i]*".Replace('true', 'false'))
+        
+#         # $pdtReindexConfContent | Out-File -FilePath "$($partialPathProjectPath)$($pdtReindex)$($partialBinPath)\$($pdtReindexCon)1"
+#     }
+
+#     # if ($pdtpndReindexConfContent -like $("*$indexes[$i]*")) {
+#     #     # Pnd Reindex
+#     #     Write-Host "Copying Pnd Reindex to $($buildsDir)" -foregroundcolor green
+#     #     & Copy-Item -Path "$($partialPathProjectPath)$($pndReindex)$($partialBinPath)" -Destination "$($buildsDir)" -Recurse
+#     #     & Rename-Item -Path "$($buildsDir)\Debug" "$($buildsDir)\$pndReindex$($i)"
+#     # }
+# }
+
+# for ($i = 0; $i -lt $indexes.Length; $i++) {
+#     if ($pdtReindexConfContent -like $("*$indexes[$i]*")) {
+#         # Pdt Reindex
+#         Write-Host "Copying Pdt Reindex to $($buildsDir)" -foregroundcolor green
+#         & Copy-Item -Path "$($partialPathProjectPath)$($pdtReindex)$($partialBinPath)" -Destination "$($buildsDir)" -Recurse
+#         & Rename-Item -Path "$($buildsDir)\Debug" "$($buildsDir)\$pdtReindex$($i)"
+#     }
+
+#     if ($pdtpndReindexConfContent -like $("*$indexes[$i]*")) {
+#         # Pnd Reindex
+#         Write-Host "Copying Pnd Reindex to $($buildsDir)" -foregroundcolor green
+#         & Copy-Item -Path "$($partialPathProjectPath)$($pndReindex)$($partialBinPath)" -Destination "$($buildsDir)" -Recurse
+#         & Rename-Item -Path "$($buildsDir)\Debug" "$($buildsDir)\$pndReindex$($i)"
+#     }
+# }
+
 # Pnd Service
-Write-Host "Copying Pnd Service to $($buildsDir)" -foregroundcolor green
-& Copy-Item -Path "$($partialPathProjectPath)$($pndService)$($partialBinPath)" -Destination "$($buildsDir)" -Recurse
-& Rename-Item -Path "$($buildsDir)\Debug" "$($buildsDir)\$pndService"
-# & Remove-Item -Path "$($buildsDir)\$pndService\$pndServiceCon"
+# Write-Host "Copying Pnd Service to $($buildsDir)" -foregroundcolor green
+# & Copy-Item -Path "$($partialPathProjectPath)$($pndService)$($partialBinPath)" -Destination "$($buildsDir)" -Recurse
+# & Rename-Item -Path "$($buildsDir)\Debug" "$($buildsDir)\$pndService"
